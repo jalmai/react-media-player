0621d54486b22ff9d5903cbdf2c3c30f58fe5809
mingman <ingman.m@gmail.com>
2024-02-22 17:27:46 +0100

    id to mapped list

--- src/components/SongListSingle.tsx
+++ src/components/SongListSingle.tsx
@@ -5,18 +5,16 @@ export function SongListSingle(): JSX.Element {
     <>
       {playlist.map(function (data) {
         return (
-          <>
-            <article className="song-item">
-              <figure>
-                <img src="assets/On_Your_Side_album_cover.jpg" alt="" />
-              </figure>
-              <div className="song-info">
-                <p className="upper-text">{data.name}</p>
-                <p className="lower-text">{data.artist}</p>
-              </div>
-              <span className="material-icons play"> play_circle_filled </span>
-            </article>
-          </>
+          <article key={data.index} className="song-item">
+            <figure>
+              <img src="assets/On_Your_Side_album_cover.jpg" alt="" />
+            </figure>
+            <div className="song-info">
+              <p className="upper-text">{data.name}</p>
+              <p className="lower-text">{data.artist}</p>
+            </div>
+            <span className="material-icons play"> play_circle_filled </span>
+          </article>
         );
       })}
     </>
