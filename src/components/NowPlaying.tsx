02ee3e031fd0fdadfc53ffe6382514f70c412abd
mingman <ingman.m@gmail.com>
2024-02-22 17:16:58 +0100

    moved code to now playng function

---
+++ src/components/NowPlaying.tsx
@@ -0,0 +1,39 @@
+export function NowPlaying(): JSX.Element {
+  return (
+    <>
+      <section className="song">
+        <figure className="album-image-container">
+          <img
+            src="assets/On_Your_Side_album_cover.jpg"
+            alt="A Rocket to the moon - album cover"
+          />
+        </figure>
+
+        <div className="song-info-container">
+          <span className="add material-icons"> add_circle_outline </span>
+          <div className="song-info">
+            <p className="upper-text">A Rocket to the mooon</p>
+            <p className="lower-text">Like we used to</p>
+          </div>
+          <span className="heart material-icons"> favorite </span>
+        </div>
+
+        <div className="time-line-container">
+          <span>0:40</span>
+          <div className="outer-time-line">
+            <div className="inner-time-line"></div>
+          </div>
+          <span>3.42</span>
+        </div>
+
+        <div className="controlls">
+          <span className="material-icons"> replay </span>
+          <span className="material-icons previous"> fast_rewind </span>
+          <span className="material-icons play"> play_circle_filled </span>
+          <span className="material-icons next"> fast_rewind </span>
+          <span className="material-icons"> shuffle </span>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,5 @@
 import { SongList } from "../components/SongList";
+import { NowPlaying } from "../components/NowPlaying";
 export function LandingPage(): JSX.Element {
   return (
     <>
@@ -9,39 +10,7 @@ export function LandingPage(): JSX.Element {
       </header>
       <main className="media-player">
         {/* TODO: Move to NowPlaying.tsx */}
-        <section className="song">
-          <figure className="album-image-container">
-            <img
-              src="assets/On_Your_Side_album_cover.jpg"
-              alt="A Rocket to the moon - album cover"
-            />
-          </figure>
-
-          <div className="song-info-container">
-            <span className="add material-icons"> add_circle_outline </span>
-            <div className="song-info">
-              <p className="upper-text">A Rocket to the mooon</p>
-              <p className="lower-text">Like we used to</p>
-            </div>
-            <span className="heart material-icons"> favorite </span>
-          </div>
-
-          <div className="time-line-container">
-            <span>0:40</span>
-            <div className="outer-time-line">
-              <div className="inner-time-line"></div>
-            </div>
-            <span>3.42</span>
-          </div>
-
-          <div className="controlls">
-            <span className="material-icons"> replay </span>
-            <span className="material-icons previous"> fast_rewind </span>
-            <span className="material-icons play"> play_circle_filled </span>
-            <span className="material-icons next"> fast_rewind </span>
-            <span className="material-icons"> shuffle </span>
-          </div>
-        </section>
+        <NowPlaying />
         <SongList />
       </main>
     </>
